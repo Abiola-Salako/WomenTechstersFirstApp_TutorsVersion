817ecf2e4c7e100f4fe32915e63bf46de33fc914
Abiola_Salako <abiolafadeelah1@gmail.com>
2023-11-26 19:32:58 +0100

    my WEEK6 ASSESSMENT SOLUTION MAINLY ON THE LOGIN ACTIVITY.KT

--- app/src/main/java/com/wtm/womentechstersfirstapp/LoginActivity.kt
+++ app/src/main/java/com/wtm/womentechstersfirstapp/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.snackbar.Snackbar
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,14 +24,29 @@ class LoginActivity : AppCompatActivity() {
             startActivity(i)
         }
 
-        val loginBtn: Button = findViewById(R.id.loginButton)
-        loginBtn.setOnClickListener{
-            navigateToLoginPage()
-        }
-    }
+        val loginBtn:Button = findViewById(R.id.loginButton)
+        val nameEdt:EditText=findViewById(R.id.username)
+        val passEdt:EditText=findViewById(R.id.password)
+        loginBtn.setOnClickListener {
+            val userNameEntered: String = nameEdt.text.toString()
+            val passwordEntered: String = passEdt.text.toString()
+            var messageRead = "";
 
-    private fun navigateToLoginPage() {
-        var intent = Intent(this, HomeActivity::class.java)
-        startActivity(intent)
+            if(userNameEntered.isEmpty() ||
+                passwordEntered.isEmpty()){
+                messageRead = "Error! both username and password are required"
+            }else{
+                var i = Intent(this, HomeActivity::class.java)
+                startActivity(i)
+            }
+              Snackbar
+                  .make(findViewById(R.id.layoutTwo),
+                      messageRead,
+                      Snackbar.LENGTH_LONG
+                  )
+                  .show()
+            }
+        }
+
+
     }
-}
